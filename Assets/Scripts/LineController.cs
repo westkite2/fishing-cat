06287010ffae5f4ba4e1143e3f74777bda275d53
westkite2 <westkite2@gmail.com>
2022-07-30 16:58:12 +0900

    Feat: Add line rewinding

--- Assets/Scripts/LineController.cs
+++ Assets/Scripts/LineController.cs
@@ -9,20 +9,31 @@ public class LineController : MonoBehaviour
     public GameObject hook;
     public Image barFill;
 
-    //플레이 모드
-    private bool isTargeting;
-
-    //던지기 속성
-    private LineRenderer lineRenderer;
-
-    private Vector2 startPosition; //던지는 방향
-    private Vector2 pressPosition;
-    private Vector2 endPosition;
-
-    private float throwPower; //던지는 힘
-    private float throwLength; //던지는 거리
-    private float throwSpeed = 5.0f; //던지는 속도
+    //모드 (exclusive)
+    private bool isTargeting; //타게팅
+    private bool isExpanding; //던지기
+    private bool isRewinding; //되감기
+    
+    //모드 보조 변수
     private float throwStartTime; //던지기 시작 시간
+    private float rewindStartTime; //되감기 시작 시간
+
+    //플레이어가 결정하는 값
+    private Vector2 pressPosition; //조준한 위치
+    private float throwPower; //던지기 힘
+
+    //다른 변수들로 계산되는 값
+    private Vector2 endPosition; //실제 던진 위치
+    private Vector2 throwDirection; //던진 선의 방향
+    private float throwAngle; //던진 선의 각도
+    private float throwLength; //던진 선의 길이 (start-end 지점 거리)
+
+    //낚싯대 특성
+    private LineRenderer lineRenderer; //낚싯대 선
+    private Vector2 startPosition; //선 초기 위치
+    private float throwSpeed = 5.0f; //던지기 속도
+    private float rewindSpeed = 6.0f; //되감기 속도
+
 
     // Start is called before the first frame update
     void Start()
@@ -35,6 +46,8 @@ public class LineController : MonoBehaviour
         hook.transform.position = startPosition;
         
         isTargeting = true;
+        isExpanding = false;
+        isRewinding = false;
 
         targetPoint.SetActive(false);
     }
@@ -44,25 +57,47 @@ public class LineController : MonoBehaviour
     {
         if (isTargeting)
         {
-            SetTargetPoint();
+            bool isDone = SetTargetPoint();
+            if (isDone)
+            {
+                //필요한 변수 값 계산
+                CalculateProperties();
+
+                //던지기 모드 시작 시간 측정
+                throwStartTime = Time.time;
+
+                //타게팅 모드에서 던지기 모드로 전환
+                isTargeting = false;
+                isExpanding = true;
+            }
         }
-        else
+        else if (isExpanding)
         {
-            ExpandLine();
+            bool isAtEndPoint = ExpandLineToEndPosition();
+            if (isAtEndPoint)
+            {
+                //되감기 모드 시작 시간 측정
+                rewindStartTime = Time.time;
 
-            
-            //RewindLine();
-            //isTargeting = true;
+                //던지기 모드에서 되감기 모드로 전환
+                isExpanding = false;
+                isRewinding = true;
+            }
         }
-
-
-
-
+        else if (isRewinding)
+        {
+            bool isAtStartPoint = RewindLineToStartPosition();
+            if (isAtStartPoint)
+            {
+                isRewinding = false;
+                isTargeting = true;
+            }
+        }
     }
 
-    private void SetTargetPoint()
+    private bool SetTargetPoint()
     {
-        //설명: 타겟 지점의 좌표와 던지는 힘 구하기
+        //설명: 타겟 지점(pressPosition)과 던지는 힘(throwPower) 구하기. 완료 시 true 반환
 
         if (Input.GetMouseButtonDown(0)) //누르는 순간 수행
         {
@@ -94,39 +129,74 @@ public class LineController : MonoBehaviour
             //타겟 포인트 표시 해제
             targetPoint.SetActive(false);
 
-            //던지기 시작 시간 측정
-            throwStartTime = Time.time;
-
-            //타게팅 모드 중지
-            isTargeting = false;
+            return true;
         }
+        return false;
     }
 
-    void ExpandLine()
+    private void CalculateProperties()
     {
-        //설명: 목표 지점까지 선을 확장
+        //설명: 던지기 및 되감기에 필요한 변수 값 계산
 
-        //던지는 지점 계산
-        Vector2 throwDirection = pressPosition - startPosition;
+        throwDirection = pressPosition - startPosition;
+        
+        throwAngle = Vector2.Angle(new Vector2(0, -1), throwDirection);
+        if (throwDirection.x < 0)
+        {
+            throwAngle = throwAngle * -1;
+        }
+
         endPosition = startPosition + (throwDirection * throwPower);
 
-        //던지기 애니메이션
         throwLength = Vector2.Distance(startPosition, endPosition);
-        float coverdLength = (Time.time - throwStartTime) * throwSpeed;
-        float coveredRatio = coverdLength / throwLength;
+
+    }
+
+    private bool ExpandLineToEndPosition()
+    {
+        //설명: 던진 위치까지 선을 확장. 던지기가 끝나면 true 반환
+
+        //선 이동
+        float coveredLength = (Time.time - throwStartTime) * throwSpeed;
+        float coveredRatio = coveredLength / throwLength;
         Vector3 newPosition = Vector3.Lerp(startPosition, endPosition, coveredRatio);
         lineRenderer.SetPosition(1, new Vector3(newPosition.x, newPosition.y, 0f));
+        
+        //갈고리 각도 설정
+        hook.transform.rotation = Quaternion.Euler(0, 0, throwAngle);
 
         //갈고리 이동
         hook.transform.position = newPosition;
 
-        //갈고리 각도 설정
-        float throwAngle = Vector2.Angle(new Vector2(0, -1), throwDirection);
-        if (throwDirection.x < 0 )
+        //던지기 완료
+        if (coveredRatio >= 1)
         {
-            throwAngle = throwAngle * -1;
+            return true;
         }
-        hook.transform.rotation = Quaternion.Euler(0, 0, throwAngle);
+        return false;
     }
 
+    private bool RewindLineToStartPosition()
+    {
+        //설명: 초기 위치까지 선을 축소. 되감기가 끝나면 true 반환
+
+        //선 이동
+        float restoredLength = (Time.time - rewindStartTime) * rewindSpeed;
+        float restoredRatio = restoredLength / throwLength;
+        Vector3 newPosition = Vector3.Lerp(endPosition, startPosition, restoredRatio);
+        lineRenderer.SetPosition(1, new Vector3(newPosition.x, newPosition.y, 0f));
+
+        //갈고리 각도 설정
+        hook.transform.rotation = Quaternion.Euler(0, 0, throwAngle);
+
+        //갈고리 이동
+        hook.transform.position = newPosition;
+
+        //되감기 완료
+        if (restoredRatio >= 1)
+        {
+            return true;
+        }
+        return false;
+    }
 }
